e99b9932823f13249c3ac4ea17f992bb6a7ece7a
anshidarinshii <anshidarinshi@gmail.com>
2025-02-02 10:24:38 +0530

    dekj

--- app.py
+++ app.py
@@ -2,5 +2,7 @@ from website import create_app
 
 if __name__ == "__main__":
     app = create_app()
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
     # whenever there will be a change in python it will auto rerun flask server
     app.run(debug=True)
